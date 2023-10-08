0e371e3f7c7391c69e3d0662d4ac6b44edb52164
Bartek <batek31221@gmail.com>
2023-10-08 17:40:56 +0200

    Game selection .2

--- Minesweeper/Commands/GenerateGameBoardCommand.cs
+++ Minesweeper/Commands/GenerateGameBoardCommand.cs
@@ -1,10 +1,14 @@
-﻿namespace Minesweeper.Commands
+﻿using System.Runtime.CompilerServices;
+
+namespace Minesweeper.Commands
 {
     public class GenerateGameBoardCommand : BaseCommand
     {
+        private int minesCount; 
+        public GenerateGameBoardCommand() { }
         public override void Execute(object? parameter)
         {
-            throw new System.NotImplementedException();
+          
         }
     }
 }
--- Minesweeper/Commands/RunPresetGameCommand.cs
+++ Minesweeper/Commands/RunPresetGameCommand.cs
@@ -1,31 +1,46 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Windows.Input;
 
 namespace Minesweeper.Commands
 {
     public class RunPresetGameCommand : BaseCommand
     {
+        private int height = 0;
+        private int width = 0;
+        private int minesCount = 0;
+
+        private readonly ICommand _GenerateGameBoardCommand;
+
         public RunPresetGameCommand()
         {
-
+            _GenerateGameBoardCommand = new GenerateGameBoardCommand();
         }
 
         public override void Execute(object? parameter)
         {
+
            switch (parameter)
             {
                 case "begginer":
-                    Debug.WriteLine("begginer");
+                    height = 8;
+                    minesCount = 10;
                     break;
 
                 case "intermediate":
-                    Debug.WriteLine("intermediate");
+                    height = 16;
+                    minesCount = 40;
                     break;
 
                 case "expert":
-                    Debug.WriteLine("expert");
+                    height = 16;
+                    width = 30;
+                    minesCount = 99;
                     break;
-                    
+            }
+
+            if(width == 0)
+            {
 
             }
         }
